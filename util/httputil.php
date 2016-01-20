3765e355f91bdcd2e37e1159eea747630975fbb3
zhangwangnan <zhang.wangnan@xiji.com>
2016-01-20 23:16:20 +0800

    20160120

--- util/httputil.php
+++ util/httputil.php
@@ -14,6 +14,12 @@ class HttpUtil{
         $value = str_replace("\\","\\\\",$value);
         $value = str_replace("'","''",$value);
         $value = str_replace('"','\"',$value);
+
+        $value = trim($value);  //清理空格
+        $value = strip_tags($value);   //过滤html标签
+        $value = htmlspecialchars($value);   //将字符内容转化为html实体
+        $value = addslashes($value);
+
         if(isset($defaultValue)&&!$value){
         	$value = $defaultValue;
         }
@@ -63,6 +69,21 @@ class HttpUtil{
             $value = str_replace("\\","\\\\",$value);
             $value = str_replace("'","''",$value);
             $value = str_replace('"','\"',$value);
+
+            $keyword = 'select|insert|update|delete|\'|\/\*|\*|\.\.\/|\.\/|union|into|load_file|outfile';
+            $arr = explode( '|', $keyword );
+            $value = str_ireplace( $arr, '', $value );
+
+            if ( !empty( $value ) ) {
+                if (!get_magic_quotes_gpc()) { // 判断magic_quotes_gpc是否为打开
+                    $value = addslashes($value); // 进行magic_quotes_gpc没有打开的情况对提交数据的过滤
+                }
+//$var = str_replace( "_", "\_", $var ); // 把 '_'过滤掉
+                $value = str_replace("%", "\%", $value); // 把 '%'过滤掉
+                $value = nl2br($value); // 回车转换
+                $value = htmlspecialchars($value); // html标记转换
+            }
+
             return $value;
         }else{
             return '';
