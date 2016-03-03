cc482849d46b6833b7ea2a6155901cc74afc2fbc
zhangwangnan <zhang.wangnan@xiji.com>
2016-03-03 19:41:37 +0800

    "数据库操作类"

---
+++ foundation/application/management/lib/mssql_db.class.php
@@ -0,0 +1,11 @@
+<?php
+    require_once 'configs_db_mssql.php';
+    class mssql_db_lib {
+        public function selectDB(){
+
+        }
+        public function saveDB(){
+
+        }
+    }
+?>
