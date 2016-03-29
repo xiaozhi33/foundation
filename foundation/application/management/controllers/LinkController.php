edcacfef12491cd4c3f9aa0275651ed32ca57c82
zhangwangnan <zhang.wangnan@xiji.com>
2016-03-29 16:53:02 +0800

    123123123

--- foundation/application/management/controllers/LinkController.php
+++ foundation/application/management/controllers/LinkController.php
@@ -5,13 +5,21 @@ require_once("../lib/mssql_db.class.php");
 class Management_linkController extends BaseController
 {
     private $dbhelper;
-    public function indexAction()
-    {
-
-    }
+    public function indexAction(){}
     public function _init()
     {
         $this->dbhelper = new mssql_db_lib();
         error_reporting("E_ALL");
     }
+    // 项目来款-推送到未认领
+    public function laikuan(){}
+
+    // 项目支出-保存到中间库中
+    public function zhichu(){}
+
+    // 新项目
+    public function new_mg(){}
+
+    // 部门同步
+    public function department_sync(){}
 }
