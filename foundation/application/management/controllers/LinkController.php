e758b4def7b72bc4e45b932daa9b4b496d7cc841
zhangwangnan <zhang.wangnan@xiji.com>
2016-03-29 15:12:15 +0800

    "link add"

---
+++ foundation/application/management/controllers/LinkController.php
@@ -0,0 +1,10 @@
+<?php
+require_once("BaseController.php");
+require_once("./../../configs_db_mssql.php");
+class Management_linkController extends BaseController
+{
+    private $dbhelper;
+    public function indexAction()
+    {
+    }
+}
