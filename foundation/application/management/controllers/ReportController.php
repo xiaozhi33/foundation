fdfbc1e89c7b8045ae55263781b262d65a31748d
wangnan <61094462@qq.com>
2016-04-06 22:47:15 +0800

    123123123

--- foundation/application/management/controllers/ReportController.php
+++ foundation/application/management/controllers/ReportController.php
@@ -593,6 +593,7 @@
 			
 			//项目名称列表
 			$pm_chouzi = new pm_mg_chouziDAO();
+            $pm_chouzi ->selectLimit .= " order by id desc";
 			$pm_chouzi = $pm_chouzi ->get($this->dbhelper);
 			$this->view->assign("pmlist",$pm_chouzi);
 		}
