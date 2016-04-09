88d73d4665afd9be3da5d98a82c012a7f8416b66
wangnan <61094462@qq.com>
2016-04-09 23:24:58 +0800

    123123123

--- foundation/index.php
+++ foundation/index.php
@@ -3,6 +3,7 @@ header("Content-type: text/html; charset=utf-8");
 date_default_timezone_set('PRC');
 	ob_start();
 	//error_reporting(0);              //notice错误 关闭错误报告
+    ini_set("display_errors", "On");
     error_reporting(E_ERROR);
     set_include_path('.' .PATH_SEPARATOR .'../library'.PATH_SEPARATOR .'./application/models/'.PATH_SEPARATOR .'./include/'.PATH_SEPARATOR .'./Models/'.PATH_SEPARATOR .'../public/'.PATH_SEPARATOR .'../util/');
 
