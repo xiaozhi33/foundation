32bcaf37a4a45a8c6b46e0fd27e697553da3a07f
wangnan <61094462@qq.com>
2016-04-21 01:05:49 +0800

    123123123

--- foundation/configs_db_mssql.php
+++ foundation/configs_db_mssql.php
@@ -5,14 +5,3 @@
  	 *  @version    1.0.1	
      */
 
-    $localhost = '127.0.0.1';
-    $rootname = 'root';
-    $passwd = '';
-    $dbname = 'test1';
-    $port = '3306';
-
-    define("__HOST__", $localhost);
-    define("__PORT__", $port);
-    define("__ROOT__", $rootname);
-    define("__PASSWD__", $passwd);
-    define("__DBNAME__", $dbname);
