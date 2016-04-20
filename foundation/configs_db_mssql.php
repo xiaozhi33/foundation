cac974c3aa6a33fd32adcc23e6b7a0655ece6272
wangnan <61094462@qq.com>
2016-04-21 01:00:43 +0800

    123123123

--- foundation/configs_db_mssql.php
+++ foundation/configs_db_mssql.php
@@ -3,16 +3,16 @@
 	 *  My new Zend Framework project
      *  @author     zhangwangnan 
  	 *  @version    1.0.1	
-     */		
+     */
 
-	$localhost = '219.243.39.69';
-	$rootname = 'tc_byjjh_zjk';
-	$passwd = 'byjjh_zjk';
-	$dbname = 'byjjh_zjk';
-	$port = '1433';
-	
-	define("__MSSQL_HOST__", $localhost);
-	define("__MSSQL_PORT__", $port);
-	define("__MSSQL_ROOT__", $rootname);
-	define("__MSSQL_PASSWD__", $passwd);
-	define("__MSSQL_DBNAME__", $dbname);
+    $localhost = '127.0.0.1';
+    $rootname = 'root';
+    $passwd = '';
+    $dbname = 'test1';
+    $port = '3306';
+
+    define("__HOST__", $localhost);
+    define("__PORT__", $port);
+    define("__ROOT__", $rootname);
+    define("__PASSWD__", $passwd);
+    define("__DBNAME__", $dbname);
