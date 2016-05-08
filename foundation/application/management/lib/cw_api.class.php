99cd025eb9e61622aa430ff1accb00d471ae9e6d
wangnan <61094462@qq.com>
2016-05-08 13:18:50 +0800

    123123123

--- foundation/application/management/lib/cw_api.class.php
+++ foundation/application/management/lib/cw_api.class.php
@@ -48,7 +48,6 @@
         {
             try{
                 $insert_SQL = "INSERT INTO zw_lkrl (lsh, rlxh, rlrq, rlr, rlrbh, bmbh, xmbh, rlje, lspz, rlpznm, czy) VALUES('$lsh', '$rlxh', '$rlrq', '$rlr', '$rlrbh', '$bmbh', '$xmbh', '$rlje', '$lspz', '$rlpznm', '$czy')";
-                alert_back($insert_SQL);exit();
                 $this->connect();
                 $rs = $this->query($insert_SQL);
                 $this ->free();
