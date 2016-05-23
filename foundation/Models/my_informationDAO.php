516e5bc30e9e9075f2f72fa16771dc4038fc7b36
zhangwangnan <zhang.wangnan@xiji.com>
2016-05-23 14:27:18 +0800

    12314143

--- foundation/Models/my_informationDAO.php
+++ foundation/Models/my_informationDAO.php
@@ -2,8 +2,8 @@
 require_once 'EasyORM.php';
 class my_informationDAO extends EasyORM{
 	 const tableName = 'my_information';
-	 const tableField ='my_infor_id,my_infor_cateid,my_infor_title,my_infor_ctitle,my_infor_sumary,my_infor_content,my_infor_datetime,my_infor_images,my_infor_isdisplay,my_infor_state';
-	 public $my_infor_id,$my_infor_cateid,$my_infor_title,$my_infor_ctitle,$my_infor_sumary,$my_infor_content,$my_infor_datetime,$my_infor_images,$my_infor_isdisplay,$my_infor_state;
+	 const tableField ='my_infor_id,my_infor_cateid,my_infor_title,my_infor_ctitle,my_infor_sumary,my_infor_content,my_infor_datetime,my_infor_images,my_infor_isdisplay,my_infor_state,my_infor_file';
+	 public $my_infor_id,$my_infor_cateid,$my_infor_title,$my_infor_ctitle,$my_infor_sumary,$my_infor_content,$my_infor_datetime,$my_infor_images,$my_infor_isdisplay,$my_infor_state,$my_infor_file;
 	 public function __construct($my_infor_id=null,$my_infor_cateid=null){
 		$this->_init(get_defined_vars());
 	 }
