e6d714889ca09949ede24ac446c0b99cc2d3cad4
zhangwangnan <zhang.wangnan@xiji.com>
2016-06-06 17:29:25 +0800

    12314143

--- foundation/application/management/controllers/ShiyongController.php
+++ foundation/application/management/controllers/ShiyongController.php
@@ -47,11 +47,10 @@
 		public function gettypebypname($pname){
 			if(!empty($pname)){
 				$pmDAO = $this->orm->createDAO("pm_mg_chouzi");
-				$pmDAO ->joinTable (" left join jjh_mg_cate as r on r.id = pm_mg_chouzi.cate");
-				$pmDAO ->selectField(" pm_mg_chouzi.*, r.catename");
+				$pmDAO ->withJjh_mg_cate(array("id"=>"cate"));
+				//$pmDAO ->select(" pm_mg_chouzi.*, r.catename");
 				$pmDAO ->findPname($pname);
 				$pmDAO = $pmDAO->get();
-				var_dump($pmDAO);exit();
 				return $pmDAO[0]['catename'];
 			}
 
