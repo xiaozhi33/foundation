d14f751d596619dc892408bb761e66309a5d9efc
zhangwangnan <zhang.wangnan@xiji.com>
2016-12-07 19:35:52 +0800

    【修改】gift car

--- foundation/application/management/controllers/CarController.php
+++ foundation/application/management/controllers/CarController.php
@@ -103,6 +103,64 @@ class Management_carController extends BaseController
 
     }
 
+    public function addusecarAction(){
+        echo $this->view->render("index/header.phtml");
+        echo $this->view->render("car/addusecar.phtml");
+        echo $this->view->render("index/footer.phtml");
+    }
+
+    public function toAddusecarAction(){
+        $id = $_REQUEST['id'];
+        $carDAO = $this->orm->createDAO('material_mg_cars');
+        $carDAO ->car_number = HttpUtil::postString("car_number");
+        $carDAO ->destination_use = HttpUtil::postString("destination_use");
+        $carDAO ->kilometers = HttpUtil::postString("kilometers");
+        $carDAO ->use_starttime = HttpUtil::postString("use_starttime");
+        $carDAO ->use_endtime = HttpUtil::postString("use_endtime");
+        $carDAO ->cost_oil_counts = HttpUtil::postString("cost_oil_counts");
+        $carDAO ->cost_price = HttpUtil::postString("cost_price");
+        $carDAO ->cost_road_toll = HttpUtil::postString("cost_road_toll");
+        $carDAO ->user = HttpUtil::postString("user");
+        $carDAO ->driver = HttpUtil::postString("driver");
+        $carDAO ->description = HttpUtil::postString("description");
+
+        if($carDAO ->car_number == ''|| $carDAO ->destination_use == ''|| $carDAO ->use_starttime == ''|| $carDAO ->use_endtime == ''){
+            //alert_back("您输入的信息不完整，请查正后继续添加！！！！！");
+            echo('<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />');
+            echo('<script language="JavaScript">');
+            echo("alert('您输入的信息不完整，请查正后继续添加！！！！！');");
+            echo('history.back();');
+            echo('</script>');
+            exit;
+        }
+        if(!empty($id))  //修改流程
+        {
+            $carDAO ->findId($id);
+        }
+        try{
+            $carDAO ->save();
+        }catch (Exception $e){
+            /*alert_back("保存失败！");
+            exit;*/
+            echo('<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />');
+            echo('<script language="JavaScript">');
+            echo("alert('保存失败！！！！！');");
+            echo('history.back();');
+            echo('</script>');
+            exit;
+        }
+
+        echo('<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />');
+        echo('<script language="JavaScript">');
+        echo("alert('保存成功');");
+        echo("location.href='/management/meeting';");
+        echo('</script>');
+        exit;
+
+        /*echo json_encode(array('msg'=>"保存成功！",'return_url'=>'/management/meeting/'));
+        exit;*/
+    }
+
     public function _init(){
         error_reporting(0);
         $carList = $this->orm->createDAO('material_mg_cars_main')->get();
