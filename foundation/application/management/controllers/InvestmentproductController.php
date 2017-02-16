ce0c49471c39337642b42c3abf4c2e7aa3bab513
zhangwangnan <zhang.wangnan@xiji.com>
2017-02-16 19:25:59 +0800

    投资管理

--- foundation/application/management/controllers/InvestmentproductController.php
+++ foundation/application/management/controllers/InvestmentproductController.php
@@ -6,31 +6,31 @@ class Management_investmentController extends BaseController
 
     public function indexAction()
     {
-        $accountDAO = $this->orm->createDAO('pm_mg_investment_account');
-        $account_name = HttpUtil::postString("account_name");
-        if(!empty($account_name)){
-            $accountDAO->findAccount_name($account_name);
+        $productDAO = $this->orm->createDAO('pm_mg_investment_product');
+        $product_name = HttpUtil::postString("product_name");
+        if(!empty($product_name)){
+            $productDAO->findproduct_name($product_name);
         }
-        $accountDAO = $accountDAO->order('id DESC');
-        $accountDAO->getPager(array('path'=>'/management/investment/index'))->assignTo($this->view);
+        $productDAO = $productDAO->order('id DESC');
+        $productDAO->getPager(array('path'=>'/management/investment/index'))->assignTo($this->view);
 
         echo $this->view->render("index/header.phtml");
         echo $this->view->render("investment/index.phtml");
         echo $this->view->render("index/footer.phtml");
     }
 
-    public function addaccountAction(){
+    public function addproductAction(){
         echo $this->view->render("index/header.phtml");
-        echo $this->view->render("car/addaccount.phtml");
+        echo $this->view->render("car/addproduct.phtml");
         echo $this->view->render("index/footer.phtml");
     }
 
-    public function toAddaccountAction(){
+    public function toAddproductAction(){
         $id = $_REQUEST['id'];
-        $accountDAO = $this->orm->createDAO('pm_mg_investment_account');
-        $account_name = HttpUtil::postString("account_name");
+        $productDAO = $this->orm->createDAO('pm_mg_investment_product');
+        $product_name = HttpUtil::postString("product_name");
 
-        if($account_name == ''){
+        if($product_name == ''){
             //alert_back("您输入的信息不完整，请查正后继续添加！！！！！");
             echo('<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />');
             echo('<script language="JavaScript">');
@@ -40,9 +40,9 @@ class Management_investmentController extends BaseController
             exit;
         }
 
-        $hasAccount = $this->hasAccount($account_name);
+        $hasproduct = $this->hasproduct($product_name);
         if(empty($id)) {
-            if ($hasAccount) {
+            if ($hasproduct) {
                 echo('<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />');
                 echo('<script language="JavaScript">');
                 echo("alert('该投资账户已添加，请核对后重新添加！！！！！');");
@@ -51,20 +51,20 @@ class Management_investmentController extends BaseController
                 exit;
             }
         }else {
-            if ($hasAccount) {
+            if ($hasproduct) {
                 echo json_encode(array('msg' => "该投资账户已添加，请核对后重新添加！！！！！！", 'return_url' => '/management/investment/'));
                 exit;
             }
         }
 
-        $accountDAO ->account_name = $account_name;
+        $productDAO ->product_name = $product_name;
 
         if(!empty($id))  //修改流程
         {
-            $accountDAO ->findId($id);
+            $productDAO ->findId($id);
         }
         try{
-            $accountDAO ->save();
+            $productDAO ->save();
         }catch (Exception $e){
             /*alert_back("保存失败！");
             exit;*/
@@ -89,35 +89,35 @@ class Management_investmentController extends BaseController
         }
     }
 
-    public function editaccountAction(){
+    public function editproductAction(){
         $id = HttpUtil::getString("id");
-        $accountDAO = $this->orm->createDAO('pm_mg_investment_account');
-        $accountDAO ->findId($id);
-        $accountDAO = $accountDAO ->get();
+        $productDAO = $this->orm->createDAO('pm_mg_investment_product');
+        $productDAO ->findId($id);
+        $productDAO = $productDAO ->get();
 
-        if($accountDAO != "")
+        if($productDAO != "")
         {
-            $this->view->assign("account_info", $accountDAO);
+            $this->view->assign("product_info", $productDAO);
             echo $this->view->render("index/header.phtml");
-            echo $this->view->render("car/editaccount.phtml");
+            echo $this->view->render("car/editproduct.phtml");
             echo $this->view->render("index/footer.phtml");
             exit();
         }
-        $accountDAO = $this->orm->createDAO('pm_mg_investment_account')->order('id DESC');
+        $productDAO = $this->orm->createDAO('pm_mg_investment_product')->order('id DESC');
 
-        $this->view->assign("account_info", $accountDAO);
+        $this->view->assign("product_info", $productDAO);
 
         echo $this->view->render("index/header.phtml");
-        echo $this->view->render("car/editaccount.phtml");
+        echo $this->view->render("car/editproduct.phtml");
         echo $this->view->render("index/footer.phtml");
         exit();
     }
 
-    public function delaccountAction(){
+    public function delproductAction(){
         $id = HttpUtil::getString("id");
-        $accountDAO = $this->orm->createDAO('pm_mg_investment_account');
-        $accountDAO ->findId($id);
-        $accountDAO = $accountDAO ->delete();
+        $productDAO = $this->orm->createDAO('pm_mg_investment_product');
+        $productDAO ->findId($id);
+        $productDAO = $productDAO ->delete();
 
         echo('<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />');
         echo('<script language="JavaScript">');
@@ -130,11 +130,11 @@ class Management_investmentController extends BaseController
     /**
      * check是否已经存在投资账户信息
      */
-    public function hasAccount($account_name){
-        $accountDAO = $this->orm->createDAO('pm_mg_investment_account');
-        $accountDAO ->findAccount_name($account_name);
-        $accountDAO = $accountDAO->get();
-        if(!empty($accountDAO)){
+    public function hasproduct($product_name){
+        $productDAO = $this->orm->createDAO('pm_mg_investment_product');
+        $productDAO ->findproduct_name($product_name);
+        $productDAO = $productDAO->get();
+        if(!empty($productDAO)){
             return true;
         }else {
             return false;
@@ -145,10 +145,10 @@ class Management_investmentController extends BaseController
 
     public function logAction()
     {
-        $logDAO = $this->orm->createDAO('pm_mg_investment_account_logs');
-        $investment_account_id = HttpUtil::postString("investment_account_id");
-        if(!empty($investment_account_id)){
-            $logDAO->findInvestment_account_id($investment_account_id);
+        $logDAO = $this->orm->createDAO('pm_mg_investment_product_logs');
+        $investment_product_id = HttpUtil::postString("investment_product_id");
+        if(!empty($investment_product_id)){
+            $logDAO->findInvestment_product_id($investment_product_id);
         }
         $logDAO = $logDAO->order('id DESC');
         $logDAO->getPager(array('path'=>'/management/investment/log'))->assignTo($this->view);
@@ -166,17 +166,17 @@ class Management_investmentController extends BaseController
 
     public function toAddlogAction(){
         $id = $_REQUEST['id'];
-        $logDAO = $this->orm->createDAO('pm_mg_investment_account_logs');
+        $logDAO = $this->orm->createDAO('pm_mg_investment_product_logs');
 
-        $investment_account_id = HttpUtil::postString("investment_account_id");         // 投资账户id
-        $account_expenses_datetime = HttpUtil::postString("account_expenses_datetime"); // 支出日期
-        $account_receipts_datetime = HttpUtil::postString("account_receipts_datetime"); // 收入时间
-        $account_receipts = HttpUtil::postString("account_receipts");                   // 收入
-        $account_expenses = HttpUtil::postString("account_expenses");                   // 支出
-        $account_corpus = HttpUtil::postString("account_corpus");   // 本金-可以先不填
+        $investment_product_id = HttpUtil::postString("investment_product_id");         // 投资账户id
+        $product_expenses_datetime = HttpUtil::postString("product_expenses_datetime"); // 支出日期
+        $product_receipts_datetime = HttpUtil::postString("product_receipts_datetime"); // 收入时间
+        $product_receipts = HttpUtil::postString("product_receipts");                   // 收入
+        $product_expenses = HttpUtil::postString("product_expenses");                   // 支出
+        $product_corpus = HttpUtil::postString("product_corpus");   // 本金-可以先不填
         $remark = HttpUtil::postString("remark");
 
-        if($investment_account_id == ''){
+        if($investment_product_id == ''){
             if(!empty($id)){
                 echo json_encode(array('msg'=>"您输入的信息不完整，请查正后继续添加！！！！！",'return_url'=>''));
                 exit;
@@ -189,12 +189,12 @@ class Management_investmentController extends BaseController
             exit;
         }
 
-        $logDAO ->investment_account_id = $investment_account_id;
-        $logDAO ->account_expenses_datetime = $account_expenses_datetime;
-        $logDAO ->account_receipts_datetime = $account_receipts_datetime;
-        $logDAO ->account_receipts = $account_receipts;
-        $logDAO ->account_expenses = $account_expenses;
-        $logDAO ->account_corpus = $account_corpus;
+        $logDAO ->investment_product_id = $investment_product_id;
+        $logDAO ->product_expenses_datetime = $product_expenses_datetime;
+        $logDAO ->product_receipts_datetime = $product_receipts_datetime;
+        $logDAO ->product_receipts = $product_receipts;
+        $logDAO ->product_expenses = $product_expenses;
+        $logDAO ->product_corpus = $product_corpus;
         $logDAO ->remark = $remark;
         if(!empty($id))  //修改流程
         {
@@ -226,7 +226,7 @@ class Management_investmentController extends BaseController
 
     public function editlogAction(){
         $id = HttpUtil::getString("id");
-        $logDAO = $this->orm->createDAO('pm_mg_investment_account_logs');
+        $logDAO = $this->orm->createDAO('pm_mg_investment_product_logs');
         $logDAO ->findId($id);
         $logDAO = $logDAO ->get();
 
@@ -238,7 +238,7 @@ class Management_investmentController extends BaseController
             echo $this->view->render("index/footer.phtml");
             exit();
         }
-        $logDAO = $this->orm->createDAO('pm_mg_investment_account_logs')->order('id DESC');
+        $logDAO = $this->orm->createDAO('pm_mg_investment_product_logs')->order('id DESC');
 
         $this->view->assign("log_info", $logDAO);
 
@@ -250,7 +250,7 @@ class Management_investmentController extends BaseController
 
     public function dellogAction(){
         $id = HttpUtil::getString("id");
-        $logDAO = $this->orm->createDAO('pm_mg_investment_account_logs');
+        $logDAO = $this->orm->createDAO('pm_mg_investment_product_logs');
         $logDAO ->findId($id);
         $logDAO ->delete();
 
@@ -264,12 +264,12 @@ class Management_investmentController extends BaseController
 
     public function _init(){
         error_reporting(0);
-        $accountList = $this->orm->createDAO('pm_mg_investment_account')->get();
+        $productList = $this->orm->createDAO('pm_mg_investment_product')->get();
         SessionUtil::sessionStart();
         SessionUtil::checkmanagement();
 
         $this->view->assign(array(
-            'accountList' => $accountList
+            'productList' => $productList
         ));
     }
 }
