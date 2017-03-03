aff9b6f215ca12b95ac33dffbfc9de1d6fbd499c
zhangwangnan <zhang.wangnan@xiji.com>
2017-03-03 11:17:57 +0800

    关闭财务对接。采用手动对接

--- foundation/application/management/controllers/AdminController.php
+++ foundation/application/management/controllers/AdminController.php
@@ -99,10 +99,10 @@
             try{
                 if($_REQUEST['name'] != "" ){
                     // 同步财务系统部门信息
-                    $deparmentlDAO = new CW_API();
+					/*$deparmentlDAO = new CW_API();
                     $rs1 = $deparmentlDAO ->get_max_departmentID();
                     $bmbh = (int)$rs1[0]['bmbh'] + 1;
-                    /*$zwbmzdlDAO = new CW_API();
+                    $zwbmzdlDAO = new CW_API();
                     $rs = $zwbmzdlDAO ->sync_department($bmbh, $_REQUEST['name']);
                     if($rs){*/
                         $departmentinfo = new jjh_mg_departmentDAO();
