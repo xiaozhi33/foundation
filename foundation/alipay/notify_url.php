7ef58f1d83ded43816be4286b6a932edc1576bec
xiaozhi33 <61094462@qq.com>
2017-06-26 13:33:39 +0800

    支付宝沙盒测试

--- foundation/alipay/notify_url.php
+++ foundation/alipay/notify_url.php
@@ -24,14 +24,22 @@ $alipaySevice = new AlipayTradeService($config);
 $alipaySevice->writeLog(var_export($_POST,true));
 $result = $alipaySevice->check($arr);
 
-// 纪录notify详情
-$notify_infoDAO = $ORM->createDAO("jjh_orders_notify_log");
-$notify_infoDAO ->jjh_orders_id = $_POST['out_trade_no'];
-$notify_infoDAO ->datetime = $_POST['notify_time'];
-$notify_infoDAO ->notify_info = json_encode($_POST);
-$notify_infoDAO ->pay_type = 3;   // 支付宝支付
-$notify_infoDAO ->other = $result;
-$notify_infoDAO ->save();
+ini_set("display_errors", "On");
+error_reporting(E_ALL);
+
+try {
+    // 纪录notify详情
+    $notify_infoDAO = $ORM->createDAO("jjh_orders_notify_log");
+    $notify_infoDAO->jjh_orders_id = $_POST['out_trade_no'];
+    $notify_infoDAO->datetime = $_POST['notify_time'];
+    $notify_infoDAO->notify_info = json_encode($_POST);
+    $notify_infoDAO->pay_type = 3;   // 支付宝支付
+    $notify_infoDAO->other = $result;
+    $notify_infoDAO->save();
+}catch (Exception $e){
+    print $e->getMessage();
+    exit();
+}
 
 /* 实际验证过程建议商户添加以下校验。
 1、商户需要验证该通知数据中的out_trade_no是否为商户系统中创建的订单号，
