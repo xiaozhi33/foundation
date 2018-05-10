82260ebb98b8df9cc9c137e51cf3018debccfc47
xiaozhi33 <61094462@qq.com>
2018-05-10 14:14:14 +0800

    111

--- foundation/application/management/controllers/IncomeController.php
+++ foundation/application/management/controllers/IncomeController.php
@@ -29,9 +29,7 @@
 
             $this->view->assign("pname", $pname);
 
-            // 按照星级倒序，之后按照创建id倒序
             $incomeinfo ->selectLimit .= " order by income_datetime desc";
-
             //$incomeinfo ->debugSql =true;
             $incomeinfo = $incomeinfo->get();
             $total = count($incomeinfo);
