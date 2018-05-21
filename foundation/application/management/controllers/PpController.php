005edb3303ee24b39f56c6a97e8cfb49b432b82c
xiaozhi33 <61094462@qq.com>
2018-05-21 15:52:19 +0800

    更新操作人

--- foundation/application/management/controllers/PpController.php
+++ foundation/application/management/controllers/PpController.php
@@ -387,7 +387,7 @@ class Management_ppController extends BaseController {
             //项目捐赠方
             $pm_ppDAO = new pm_mg_infoDAO();
             $pm_ppDAO ->joinTable(" left join pm_mg_chouzi as c on pm_mg_info.pm_name=c.pname");
-            $pm_ppDAO ->selectField(" c.id, distinct c.pname");
+            $pm_ppDAO ->selectField(" c.id, DISTINCT c.pname");
             $pm_ppDAO ->selectLimit .= ' AND pm_mg_info.pm_pp ='.$ppinfo['ppname'];
             $pm_ppDAO ->selectLimit .= ' ORDER BY c.id desc';
             $pm_ppDAO = $pm_ppDAO->get($this->dbhelper);
