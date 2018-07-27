21fd33a3c53b704671a72be07af04da796bd9177
xiaozhi33 <61094462@qq.com>
2018-07-27 22:24:24 +0800

    20180727

--- foundation/Models/my_categoryDAO.php
+++ foundation/Models/my_categoryDAO.php
@@ -2,7 +2,7 @@
 require_once 'EasyORM.php';
 class my_categoryDAO extends EasyORM{
 	 const tableName = 'my_category';
-	 const tableField ='c_id,c_name,c_parent_id,c_path,c_describe,c_content,c_images';
+	 const tableField ='c_id,c_name,c_parent_id,c_path,c_describe,c_content,c_images,c_online';
 	 public $c_id,$c_name,$c_parent_id,$c_path,$c_describe,$c_content,$c_images;
 	 public function __construct($c_id=null){
 		$this->_init(get_defined_vars());
