48db383370107482b01769d86e45f0c997c7529e
xiaozhi33 <61094462@qq.com>
2018-11-13 11:28:31 +0800

    20181108 项目详情导出

--- foundation/application/management/controllers/ReportController.php
+++ foundation/application/management/controllers/ReportController.php
@@ -2001,7 +2001,7 @@
 			$pm_mg_chouzi ->withJjh_mg_cate(array("cate" => "id"));
 			$pm_mg_chouzi ->withJjh_mg_department(array("department" => "id"));
 			$pm_mg_chouzi ->withPm_mg_rate(array('id' => "pm_id"));
-			$pm_mg_chouzi ->withJjh_mg_pp(array('pid' => "pm_fzr"));
+			$pm_mg_chouzi ->withJjh_mg_pp(array('pm_fzr' => "pid"));
 			$pm_mg_chouzi ->select(" pm_mg_chouzi.*, jjh_mg_cate.catename, jjh_mg_department.pname as department_name, pm_mg_rate.pm_rate");
 
 			// 过滤逻辑删除的项目
