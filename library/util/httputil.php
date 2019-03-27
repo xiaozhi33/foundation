b8e7c23ce78792652569217edcc4c92878b7f258
xiaozhi33 <61094462@qq.com>
2019-03-27 23:01:41 +0800

    系统漏洞修复 20190327

--- library/util/httputil.php
+++ library/util/httputil.php
@@ -72,8 +72,19 @@ class HttpUtil{
             $value = str_replace("'","''",$value);
             $value = str_replace('"','\"',$value);
 
-            $keyword = 'select|insert|update|delete|\'|\/\*|\*|\.\.\/|\.\/|union|into|load_file|outfile';
+            $keyword = 'select|insert|update|delete|\'|\/\*|\*|\.\.\/|\.\/|union|into|load_file|outfile|script|document|eval|<|>';
             $arr = explode( '|', $keyword );
+
+            // 如果含有非法信息，直接跳转到404页面
+            foreach ($arr as $k => $v){
+                if(check_str($v,$value)){
+                    @header("http/1.1 404 not found");
+                    @header("status: 404 not found");
+                    include("http://www.phpernote.com/404.html");//跳转到某一个页面，推荐使用这种方法
+                    exit();
+                }
+            }
+
             $value = str_ireplace( $arr, '', $value );
 
             if ( !empty( $value ) ) {
