72c07bf33acfa72560abc3d4464ffb3f8b070b47
xiaozhi33 <61094462@qq.com>
2019-03-27 23:36:10 +0800

    系统漏洞修复 20190327

--- foundation/application/default/controllers/NewsController.php
+++ foundation/application/default/controllers/NewsController.php
@@ -2,35 +2,41 @@
 	require_once("BaseController.php");
 	require_once("../util/httputil.php");
 	require_once("../util/sessionutil.php");
+
+	error_reporting(E_ERROR);
 	
 	class newsController extends BaseController {
 		private $dbhelper;
 		public function indexAction(){
-			$cid = HttpUtil::getString("cid");
-			$information = new my_informationDAO(null,$cid);
-			$information ->selectLimit = " and my_infor_isdisplay = 1 and my_infor_state = 1 order by my_infor_datetime desc";
-			$information = $information ->get($this->dbhelper);
-			
-			$total = count($information);
-			$pageDAO = new pageDAO();
-			$pageDAO = $pageDAO ->pageHelper($information,null,"index",null,'get',14,14);						
-			$pages = $pageDAO['pageLink']['all'];
-			$pages = str_replace("/index.php","",$pages);	
-			$this->view->assign('informationlist',$pageDAO['pageData']);
-			$this->view->assign('page',$pages);	
-			$this->view->assign('total',$total);
-			
-			if($cid == "gonggao" || $cid == ""){
-				$c_name = "基金会公告";
-			}elseif ($cid == "zizhuxinxi"){
-				$c_name = "资助信息";
-			}elseif ($cid == "xinwen"){
-				$c_name = "基金会新闻";
-			}elseif ($cid == "gongshi"){
-				$c_name = "捐赠公示";
-			}		
-			$this->view->assign('c_name',$c_name);
-			echo $this->view->render("news/index.phtml");
+			try{
+				$cid = HttpUtil::getString("cid");
+				$information = new my_informationDAO(null,$cid);
+				$information ->selectLimit = " and my_infor_isdisplay = 1 and my_infor_state = 1 order by my_infor_datetime desc";
+				$information = $information ->get($this->dbhelper);
+
+				$total = count($information);
+				$pageDAO = new pageDAO();
+				$pageDAO = $pageDAO ->pageHelper($information,null,"index",null,'get',14,14);
+				$pages = $pageDAO['pageLink']['all'];
+				$pages = str_replace("/index.php","",$pages);
+				$this->view->assign('informationlist',$pageDAO['pageData']);
+				$this->view->assign('page',$pages);
+				$this->view->assign('total',$total);
+
+				if($cid == "gonggao" || $cid == ""){
+					$c_name = "基金会公告";
+				}elseif ($cid == "zizhuxinxi"){
+					$c_name = "资助信息";
+				}elseif ($cid == "xinwen"){
+					$c_name = "基金会新闻";
+				}elseif ($cid == "gongshi"){
+					$c_name = "捐赠公示";
+				}
+				$this->view->assign('c_name',$c_name);
+				echo $this->view->render("news/index.phtml");
+			}catch(Exception $e){
+				throw $e;
+			}
 		}
 		
 		public function newsinfoAction(){
