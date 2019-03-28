cabf03092725e058d497879f2be81265a2476b34
xiaozhi33 <61094462@qq.com>
2019-03-28 10:14:30 +0800

    系统漏洞修复 20190328

--- foundation/application/default/controllers/NewsController.php
+++ foundation/application/default/controllers/NewsController.php
@@ -73,6 +73,14 @@
 				exit();
 			}
 
+			$request_body = file_get_contents('php://input');
+			if(!empty($request_body)){
+				@header("http/1.1 404 not found");
+				@header("status: 404 not found");
+				include("http://www.phpernote.com/404.html");//跳转到某一个页面，推荐使用这种方法
+				exit();
+			}
+
 			if (isset($GLOBALS['HTTP_RAW_POST_DATA'])) {
 				@header("http/1.1 404 not found");
 				@header("status: 404 not found");
