1dff485557a187e64184310a60c9cd465b05aed6
xiaozhi33 <61094462@qq.com>
2019-03-28 09:44:23 +0800

    系统漏洞修复 20190328

--- util/httputil.php
+++ util/httputil.php
@@ -70,7 +70,7 @@ class HttpUtil{
                 $value = str_replace("'","''",$value);
                 $value = str_replace('"','\"',$value);
 
-                $keyword = 'select|insert|update|delete|\'|\/\*|\*|\.\.\/|\.\/|union|into|load_file|outfile|script|document|eval|<|>|$|%|@|\"|;|(|)|,|.|CR|LF|+|alert';
+                $keyword = 'select|insert|update|delete|\'|\/\*|\*|\.\.\/|\.\/|union|into|load_file|outfile|script|document|eval|<|>|$|%|@|\"|;|(|)|,|.|CR|LF|+|alert|iframe|frame';
                 $arr = explode( '|', $keyword );
 
                 // 如果含有非法信息，直接跳转到404页面
